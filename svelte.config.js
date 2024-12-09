68e7dfbe28cca09a13e5f8ac83924bc1a7ba5908
Jasper Palfree <well.caffeinated@gmail.com>
2024-12-09 13:49:16 -0700

    chore: setup prettier code formatting

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,28 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import { mdsvex } from "mdsvex";
-import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import remarkMath from "remark-math";
-import rehypeKatexSvelte from "rehype-katex-svelte";
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { mdsvex } from 'mdsvex'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import remarkMath from 'remark-math'
+import rehypeKatexSvelte from 'rehype-katex-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: [".svelte", ".md"],
+  extensions: ['.svelte', '.md'],
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: [
     vitePreprocess(),
     mdsvex({
-      extensions: [".md"],
+      extensions: ['.md'],
       layout: {
-        book: "./src/markdown-layouts/book.svelte",
-        _: "./src/markdown-layouts/book.svelte",
+        book: './src/markdown-layouts/book.svelte',
+        _: './src/markdown-layouts/book.svelte',
       },
       highlight: {
         highlighter: (code, lang) => {
-          let escaped = code.replace(/\\/g, "\\\\").replace(/`/g, "\\`");
-          return `<Components.CodeBlock language="${lang}" code={\`${escaped}\`}/>`;
+          let escaped = code.replace(/\\/g, '\\\\').replace(/`/g, '\\`')
+          return `<Components.CodeBlock language="${lang}" code={\`${escaped}\`}/>`
         },
       },
       remarkPlugins: [remarkMath],
@@ -32,8 +32,8 @@ const config = {
   kit: {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      fallback: 'index.html' // may differ from host to host
+      fallback: 'index.html', // may differ from host to host
     }),
   },
-};
-export default config;
+}
+export default config
