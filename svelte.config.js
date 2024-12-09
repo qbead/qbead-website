821fe378a6444acddb1cd9e23d595f9e3709f163
Jasper Palfree <well.caffeinated@gmail.com>
2024-12-09 12:34:13 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -32,7 +32,7 @@ const config = {
   kit: {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      fallback: '200.html' // may differ from host to host
+      fallback: 'index.html' // may differ from host to host
     }),
   },
 };
