456d2b29dc80baadfd8240cdd03563ac2e46fe97
Jasper Palfree <well.caffeinated@gmail.com>
2025-04-20 16:47:03 -0600

    simple sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,80 @@
 <script lang="ts">
 	import '../app.css';
-	import { AppBar } from '@skeletonlabs/skeleton-svelte';
-	import ArrowLeft from '@lucide/svelte/icons/arrow-left';
-	import Paperclip from '@lucide/svelte/icons/paperclip';
-	import Calendar from '@lucide/svelte/icons/calendar';
-	import CircleUser from '@lucide/svelte/icons/circle-user';
+	import { Navigation, Avatar } from '@skeletonlabs/skeleton-svelte'
+  // Icons
+  import IconMenu from '@lucide/svelte/icons/menu'
+  import IconFolder from '@lucide/svelte/icons/folder'
+  import IconBook from '@lucide/svelte/icons/book'
+  import IconInfo from '@lucide/svelte/icons/info'
+  import IconGames from '@lucide/svelte/icons/gamepad'
+	import { page } from '$app/state'
 
-	let { children } = $props();
+  let isExpansed = $state(true)
+
+  function toggleExpanded() {
+    isExpansed = !isExpansed
+  }
+
+	const routes: { [key: string]: string } = {
+		'/lessons': 'lessons',
+		'/playground': 'playground',
+		'/about': 'about'
+	}
+
+	const getCurrentRoute = (path: string): string => {
+		return Object.entries(routes).find(([key]) => {
+			return path.startsWith(key)
+		})?.at(1) || 'home'
+	}
+
+	let currentRoute: string = $derived(getCurrentRoute(page.url.pathname))
+	$inspect('currentRoute', currentRoute)
+
+	let { children } = $props()
 </script>
 
-<AppBar>
-	{#snippet lead()}
-		<ArrowLeft size={24} />
-	{/snippet}
-	{#snippet trail()}
-		<Paperclip size={20} />
-		<Calendar size={20} />
-		<CircleUser size={20} />
-	{/snippet}
-	<span>Title</span>
-</AppBar>
-<section class="container">
-	{@render children()}
-</section>
+<div class="grid grid-cols-[auto_1fr]">
+  <!-- Sidebar -->
+  <aside class="sticky top-0 col-span-1 h-screen">
+		<Navigation.Rail expanded={isExpansed} tilesJustify="top" value={currentRoute}>
+			{#snippet header()}
+				<Navigation.Tile
+					labelExpanded="Menu"
+					onclick={toggleExpanded}
+					title="Toggle Menu Width"
+				>
+					<IconMenu />
+				</Navigation.Tile>
+			{/snippet}
+			{#snippet tiles()}
+				<Navigation.Tile labelExpanded="Home" href="/" id="home">
+					<Avatar name="icon">
+						<IconFolder />
+					</Avatar>
+				</Navigation.Tile>
+				<Navigation.Tile labelExpanded="Lessons" href="/lessons" id="lessons">
+					<Avatar name="icon">
+					 	<IconBook />
+					</Avatar>
+				</Navigation.Tile>
+				<Navigation.Tile labelExpanded="The Playground" href="/playground" id="playground">
+					<Avatar name="icon">
+						<IconGames />
+					</Avatar>
+				</Navigation.Tile>
+				<Navigation.Tile labelExpanded="About Us" href="/about" id="about">
+					<Avatar name="icon">
+						<IconInfo />
+					</Avatar>
+				</Navigation.Tile>
+			{/snippet}
+			<!-- {#snippet footer()}
+				<Navigation.Tile labelExpanded="Settings" href="/settings" title="Settings"><IconSettings /></Navigation.Tile>
+			{/snippet} -->
+		</Navigation.Rail>
+	</aside>
+  <!-- Main -->
+  <main class="col-span-1 p-4">
+  	{@render children()}
+  </main>
+</div>
--- src/routes/lessons/introduction.md
+++ src/routes/lessons/introduction.md
@@ -64,3 +64,13 @@ void loop() {
 ```
 
 <button type="button" class="btn preset-filled-primary-500">test button</button>
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
