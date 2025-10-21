8eaff66518bf82214159607e3225ebf2bb954740
Jasper Palfree <well.caffeinated@gmail.com>
2025-10-21 15:21:50 -0400

    style fixes for literate layout

--- src/routes/code/+layout.svelte
+++ src/routes/code/+layout.svelte
@@ -1,6 +1,5 @@
 <div class="layout-literate">
   <article class="typography prose prose-lg max-w-full py-36">
-    <div class="lit-bg"></div>
     <slot>
       <!-- the content will be slotted in here -->
     </slot>
