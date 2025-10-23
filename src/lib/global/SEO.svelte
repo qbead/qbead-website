d9e6bf85dd96d4eb61be36e1b08d500666d2c5ac
Jasper Palfree <well.caffeinated@gmail.com>
2025-10-22 20:47:46 -0400

    seo adjustments

--- src/lib/global/SEO.svelte
+++ src/lib/global/SEO.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
-  import SITE_META from './site-metadata.json'
+  import {
+    PUBLIC_DEFAULT_TITLE,
+    PUBLIC_DEFAULT_DESCRIPTION,
+    PUBLIC_TITLE_TEMPLATE,
+  } from '$env/static/public'
   import { page } from '$app/state'
 
-  let title = page.data?.metadata?.title
-  let description = page.data?.metadata?.description
-  let robots =
-    page.data?.metadata?.robots === false ? 'noindex, nofollow' : page.data?.metadata?.robots
+  let title = $derived(page.data?.metadata?.title)
+  let description = $derived(page.data?.metadata?.description)
 
-  let metaTitle = $derived(
-    title ? SITE_META.titleTemplate.replace('%s', title) : SITE_META.defaultTitle
+  let robots = $derived(
+    page.data?.metadata?.robots === false ? 'noindex, nofollow' : page.data?.metadata?.robots
   )
 
-  let metaDescription = $derived(description ? description : SITE_META.defaultDescription)
+  let metaTitle = $derived(
+    title ? PUBLIC_TITLE_TEMPLATE.replace('%s', title) : PUBLIC_DEFAULT_TITLE
+  )
+
+  let metaDescription = $derived(description ? description : PUBLIC_DEFAULT_DESCRIPTION)
 </script>
 
 <svelte:head>
