ea5f5e79a325cce9abd5faad794dad2961749646
Jasper Palfree <well.caffeinated@gmail.com>
2025-11-07 16:16:18 -0500

    ability to add custom icon to callout

--- src/lib/components/Callout/Callout.svelte
+++ src/lib/components/Callout/Callout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { type Icon } from '@lucide/svelte'
   import IconAlert from '@lucide/svelte/icons/alert-circle'
   import IconInfo from '@lucide/svelte/icons/info'
   import IconQuestion from '@lucide/svelte/icons/circle-question-mark'
@@ -7,40 +8,57 @@
   let {
     title,
     type = 'info',
+    icon,
+    iconColor,
+    color,
     children,
   } = $props<{
     title?: string
     type?: 'info' | 'plain' | 'alert' | 'question' | 'book'
+    icon?: typeof Icon
+    iconColor?: string
+    color?: string
     children: () => any
   }>()
 
   const icons: any = {
     info: {
-      color: 'text-primary-500',
+      color: color ?? 'text-primary-500',
+      iconColor: iconColor ?? color ?? 'text-primary-500',
       component: IconInfo,
     },
     question: {
-      color: 'text-secondary-500',
+      color: color ?? 'text-secondary-500',
+      iconColor: iconColor ?? color ?? 'text-secondary-500',
       component: IconQuestion,
     },
     alert: {
-      color: 'text-warning-500',
+      color: color ?? 'text-warning-500',
+      iconColor: iconColor ?? color ?? 'text-warning-500',
       component: IconAlert,
     },
     book: {
-      color: 'text-secondary-500',
+      color: color ?? 'text-secondary-500',
+      iconColor: iconColor ?? color ?? 'text-secondary-500',
       component: BookOpen,
     },
   }
 
   let IconChoice = $derived.by(() => {
+    if (icon) {
+      return {
+        color,
+        iconColor: iconColor ?? color,
+        component: icon,
+      }
+    }
     return icons[type]
   })
 </script>
 
-{#snippet icon(IconChoice: any)}
+{#snippet iconBlock(IconChoice: any)}
   {#if type !== 'plain'}
-    <div class="bg-surface-200-800 rounded-2xl p-3 {IconChoice.color}">
+    <div class="bg-surface-200-800 rounded-2xl p-3 {IconChoice.iconColor}">
       <IconChoice.component size="32" />
     </div>
   {/if}
@@ -49,7 +67,7 @@
 <aside class="bg-surface-100-900 text-surface-700-300 mb-8 flex flex-col gap-8 rounded-lg p-8">
   {#if title}
     <div class="h4 flex items-center gap-6 {IconChoice.color}">
-      {@render icon(IconChoice)}
+      {@render iconBlock(IconChoice)}
       <span>{title}</span>
     </div>
     <div class="content">
@@ -57,7 +75,7 @@
     </div>
   {:else}
     <div class="flex items-start gap-6">
-      {@render icon(IconChoice)}
+      {@render iconBlock(IconChoice)}
       <div class="content flex-1 pt-3">
         {@render children()}
       </div>
