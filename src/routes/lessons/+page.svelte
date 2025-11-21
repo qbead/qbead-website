6a55aa890f289c02e999cc1b8807ea2ef37dbcb2
Stefan Krastanov <stefan@krastanov.org>
2025-11-20 23:47:25 -0500

    mark the learning guide page as draft to avoid people looking at it too much until we polish it

--- src/routes/lessons/+page.svelte
+++ src/routes/lessons/+page.svelte
@@ -112,7 +112,7 @@
 <div class="lesson-list-layout m-auto my-32 max-w-7xl px-10">
   <div class="head-area">
     <FancyHeading
-      title="The Learning Guide"
+      title="The Learning Guide (unfinished draft)"
       highlightText="Learning Guide"
       altText="Play"
       class="lg:mb-20"
