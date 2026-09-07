3f65dc6b8d7ecc9ce013fe1469b9dc9e3ca3bf4b
carloserher <c.errandoherranz@tudelft.nl>
2026-09-07 16:35:57 +0200

    minor text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,7 +52,7 @@
     <div class="flex flex-col gap-10">
       <div class="flex flex-wrap gap-4">
         <a href="/about" class="btn btn-lg preset-filled-primary-500"> Discover the Qbead </a>
-        <a href="/lessons" class="btn btn-lg preset-filled-surface-100-900"> Explore Lessons </a>
+        <a href="/lessons" class="btn btn-lg preset-filled-surface-100-900"> Explore our Lessons </a>
       </div>
       <ul class="flex flex-wrap gap-4">
         <li class="flex items-center">
